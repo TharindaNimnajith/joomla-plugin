f73be6b1fa5ead5aec5729a484a8cb72b11a9916
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2022-07-24 12:38:48 +0530

    init

--- script.php
+++ script.php
@@ -23,27 +23,33 @@ class plgSystemJoomla_pluginInstallerScript
 	 *
 	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
 	 */
-	public function __construct(JAdapterInstance $adapter) {}
+	public function __construct(JAdapterInstance $adapter)
+	{
+	}
 
 	/**
 	 * Called before any type of action
 	 *
-	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
+	 * @param   string            $route    Which action is happening (install|uninstall|discover_install|update)
 	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
 	 *
 	 * @return  boolean  True on success
 	 */
-	public function preflight($route, JAdapterInstance $adapter) {}
+	public function preflight($route, JAdapterInstance $adapter)
+	{
+	}
 
 	/**
 	 * Called after any type of action
 	 *
-	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
+	 * @param   string            $route    Which action is happening (install|uninstall|discover_install|update)
 	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
 	 *
 	 * @return  boolean  True on success
 	 */
-	public function postflight($route, JAdapterInstance $adapter) {}
+	public function postflight($route, JAdapterInstance $adapter)
+	{
+	}
 
 	/**
 	 * Called on installation
@@ -52,7 +58,9 @@ class plgSystemJoomla_pluginInstallerScript
 	 *
 	 * @return  boolean  True on success
 	 */
-	public function install(JAdapterInstance $adapter) {}
+	public function install(JAdapterInstance $adapter)
+	{
+	}
 
 	/**
 	 * Called on update
@@ -61,12 +69,16 @@ class plgSystemJoomla_pluginInstallerScript
 	 *
 	 * @return  boolean  True on success
 	 */
-	public function update(JAdapterInstance $adapter) {}
+	public function update(JAdapterInstance $adapter)
+	{
+	}
 
 	/**
 	 * Called on uninstallation
 	 *
 	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
 	 */
-	public function uninstall(JAdapterInstance $adapter) {}
+	public function uninstall(JAdapterInstance $adapter)
+	{
+	}
 }
