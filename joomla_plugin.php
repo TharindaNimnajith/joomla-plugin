da4822e8e120e81194a6dcfb6006b2a14d069b34
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2022-07-24 12:38:06 +0530

    init

---
+++ joomla_plugin.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * @package    joomla_plugin
+ *
+ * @author     ASUS <your@email.com>
+ * @copyright  A copyright
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       http://your.url.com
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Plugin\CMSPlugin;
+
+/**
+ * Joomla_plugin plugin.
+ *
+ * @package   joomla_plugin
+ * @since     1.0.0
+ */
+class plgSystemJoomla_plugin extends CMSPlugin
+{
+	/**
+	 * Application object
+	 *
+	 * @var    CMSApplication
+	 * @since  1.0.0
+	 */
+	protected $app;
+
+	/**
+	 * Database object
+	 *
+	 * @var    JDatabaseDriver
+	 * @since  1.0.0
+	 */
+	protected $db;
+
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var    boolean
+	 * @since  1.0.0
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * onAfterInitialise.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterInitialise()
+	{
+
+	}
+
+	/**
+	 * onAfterRoute.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterRoute()
+	{
+
+	}
+
+	/**
+	 * onAfterDispatch.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterDispatch()
+	{
+
+	}
+
+	/**
+	 * onAfterRender.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterRender()
+	{
+		// Access to plugin parameters
+		$sample = $this->params->get('sample', '42');
+	}
+
+	/**
+	 * onAfterCompileHead.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterCompileHead()
+	{
+
+	}
+
+	/**
+	 * OnAfterCompress.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterCompress()
+	{
+
+	}
+
+	/**
+	 * onAfterRespond.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterRespond()
+	{
+
+	}
+}
